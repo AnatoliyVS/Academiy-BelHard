ac8f0fe3c30116d1bd01d305ce12a30b50d3d0a0
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-22 14:20:54 +0300

    commit method main

--- src/practicaltasks/task002/Main.java
+++ src/practicaltasks/task002/Main.java
@@ -3,6 +3,7 @@ package practicaltasks.task002;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Scanner;
 import practicaltasks.task002.datetime.DateTime;
 
 /**
@@ -16,20 +17,79 @@ import practicaltasks.task002.datetime.DateTime;
  */
 public class Main {
 
+    private static boolean flag;
+
     public static void main(String[] args) {
 
+        do {
+            {//-----------------------------------------------------------------
+                ProgramIsRunning();
+            }//-----------------------------------------------------------------
+
+            do {
+                switch (menu()) {
+                    case "1":
+                        flag = true;
+                        break;
+                    case "2": {
+                        System.exit(0);
+                    }
+                    default: {
+                        System.err.println(
+                                "Не правильно выбрали номер из меню, повторите пожалуйста"
+                        );
+                        flag = false;
+                    }
+                }
+            } while (flag == false);
+
+        } while (true);
+
+    }
+
+    private static String menu() {
+
+        System.out.println();
+
+        System.out.println(
+                "Меню:" + "\n"
+                + "1 - повторит" + "\n"
+                + "2 - выход"
+        );
+
+        System.out.print("> ");
+
+        Scanner inputMenu = new Scanner(System.in);
+
+        switch (inputMenu.nextLine()) {
+            case "1":
+                return "1";
+            case "2":
+                return "2";
+        }
+        return "0";
+    }
+
+    private static void ProgramIsRunning() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите время в миллисекундах прошедшее с момента 1 января 1970 года 00:00: ");
+        long inputMilliseconds = input.nextInt();
+
 //==============================================================================
-        Date date = new Date();
-        DateTime dateTime = new DateTime(date.getTime());
-        System.out.println(dateTime.getDate());
-        System.out.println("Time: " + date.getTime());
+//        Date date = new Date();
+//        DateTime dateTime = new DateTime(date.getTime());
+//        System.out.println(dateTime.getDate());
+//        System.out.println("Time: " + date.getTime());
 //==============================================================================
 //        DateTime dateTime = new DateTime(System.currentTimeMillis());
 //        System.out.println(dateTime.getDate());
-//        Locale local = new Locale("ru", "RU");
 //==============================================================================
 //
 //==============================================================================
+//        Locale local = new Locale("ru", "RU");
+//
 //        DateFormat df1 = DateFormat.getDateInstance(DateFormat.LONG, local);
 //        Date date = new Date();
 //        System.out.println("Date: " + df1.format(date));
