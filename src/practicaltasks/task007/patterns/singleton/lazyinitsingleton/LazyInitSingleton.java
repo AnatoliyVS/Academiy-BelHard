8238420f0406c3b24fde40fcefbfce96522f94b9
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-26 23:54:36 +0300

    Design pattern

---
+++ src/practicaltasks/task007/patterns/singleton/lazyinitsingleton/LazyInitSingleton.java
@@ -0,0 +1,16 @@
+package practicaltasks.task007.patterns.singleton.lazyinitsingleton;
+
+/**
+ * Шаблон проектирования Singleton.
+ *
+ * Класс LazyInitSingleton, Singleton с ленивой инициализацией/
+ *
+ * Ленивая инициализация — способ реализации шаблона Singleton с глобальной
+ * методом доступа к экземпляру класса.
+ *
+ * @author Anatoliy
+ */
+public class LazyInitSingleton {
+
+    
+}
