cd95bc398ebbbb04f1bad01a5dbbdf0a94d9b4a8
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-27 00:04:16 +0300

    Lazy Initialization

--- src/practicaltasks/task007/patterns/singleton/lazyinitsingleton/LazyInitSingleton.java
+++ src/practicaltasks/task007/patterns/singleton/lazyinitsingleton/LazyInitSingleton.java
@@ -3,7 +3,7 @@ package practicaltasks.task007.patterns.singleton.lazyinitsingleton;
 /**
  * Шаблон проектирования Singleton.
  *
- * Класс LazyInitSingleton, Singleton с ленивой инициализацией/
+ * Класс LazyInitSingleton, Singleton с ленивой инициализацией
  *
  * Ленивая инициализация — способ реализации шаблона Singleton с глобальной
  * методом доступа к экземпляру класса.
@@ -13,12 +13,12 @@ package practicaltasks.task007.patterns.singleton.lazyinitsingleton;
 public class LazyInitSingleton {
 
     private static LazyInitSingleton instance;
-    
+
     private LazyInitSingleton() {
     }
-    
+
     public static LazyInitSingleton getInstanse() {
-        
+
         return (instance == null)
                 ? instance = new LazyInitSingleton()
                 : instance;
