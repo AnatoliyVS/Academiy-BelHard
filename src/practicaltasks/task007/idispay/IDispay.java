6933bd884ec4b531b8278f4e6bc913f5f3f9e031
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-29 15:59:30 +0300

    interface IDisplay

---
+++ src/practicaltasks/task007/idispay/IDispay.java
@@ -0,0 +1,13 @@
+package practicaltasks.task007.idispay;
+
+/**
+ *
+ * @author Anatoliy
+ */
+public interface IDispay {
+
+    public String TAB = "\t";
+    public String STR = "Что-то делает.";
+
+    public void disply();
+}
