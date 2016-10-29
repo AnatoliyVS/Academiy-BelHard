c658974b53323c6f3ed8be274c66d877f9a03f65
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-29 20:45:35 +0300

    class Main

--- src/practicaltasks/task008/Main.java
+++ src/practicaltasks/task008/Main.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicaltasks.task008;
 
 /**
+ * сделть неизменяемый класс Service, в котором есть поля-мяссив объектов
+ * Service
  *
  * @author Anatoliy
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
+
 }
