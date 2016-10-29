6dab9674137b680c368811c89f4779acc3431e04
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-30 00:32:04 +0300

    commit task #8

--- src/practicaltasks/task008/Main.java
+++ src/practicaltasks/task008/Main.java
@@ -1,16 +1,45 @@
 package practicaltasks.task008;
 
+import java.util.Arrays;
+import practicaltasks.task008.service.Service;
+
 /**
  * сделть неизменяемый класс Service, в котором есть поля-мяссив объектов
  * Service
  *
- * @author Anatoliy
+ * @author Стесик Анатолий
  */
 public class Main {
 
+    private static final int SIZE = 5;
+    private static Service[] services;
+    private static Service[] services1;
+
     public static void main(String[] args) {
-        
-        
+
+        services = new Service[SIZE];
+
+        for (int i = 0; i < services.length; i++) {
+            services[i] = new Service();
+        }
+
+        for (Service service : services) {
+            System.out.print(service.toString());
+        }
+
+        System.out.println();
+
+        services1[SIZE] = new Service(services);
+
+        for (int i = 0; i < services1.length; i++) {
+            services1[i] = new Service(services);
+        }
+
+        for (Service services11 : services1) {
+            System.out.print(services11.toString());
+        }
+
+        System.out.println();
     }
 
 }
---
+++ src/practicaltasks/task008/service/Service.java
@@ -0,0 +1,19 @@
+package practicaltasks.task008.service;
+
+/**
+ *
+ * @author Стесик Анатолий
+ */
+public final class Service {
+
+    public final Service[] services;
+
+    public Service() {
+        services = new Service[0];
+    }
+
+    public Service(Service[] services) {
+        this.services = services;
+    }
+
+}
