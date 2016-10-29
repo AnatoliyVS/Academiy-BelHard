6cc946a4024b36ee69c2a7beefb0e518c2697781
AnatoliyVS <anatoliyfx2@yandex.ru>
2016-10-29 15:58:38 +0300

    examples using pattern Singleton

--- src/practicaltasks/task007/Main.java
+++ src/practicaltasks/task007/Main.java
@@ -1,22 +1,35 @@
 package practicaltasks.task007;
 
 import practicaltasks.task007.patterns.singleton.eagerinit.EagerInitSingleton;
+import practicaltasks.task007.patterns.singleton.enumsingleton.EnumSingleton;
 import practicaltasks.task007.patterns.singleton.lazyinit.LazyInitSingleton;
 
 /**
  *
- * @author Anatoliy
+ * @author Стесик Анатолий
  */
 public class Main {
 
     public static void main(String[] args) {
 
-        LazyInitSingleton lazyInitSingleton = LazyInitSingleton.getInstanse();
-        System.out.println(lazyInitSingleton.toString());
-
-        EagerInitSingleton eagerInitSingleton = EagerInitSingleton.getInstance();
-        System.out.println(eagerInitSingleton.toString());
-
+        // 
+        LazyInitSingleton.getInstanse().disply();
+        LazyInitSingleton.getInstanse().disply();
+        LazyInitSingleton.getInstanse().disply();
+        
+        System.out.println();
+        
+        EagerInitSingleton.getInstance().disply();
+        EagerInitSingleton.getInstance().disply();
+        EagerInitSingleton.getInstance().disply();
+        
+        System.out.println();
+        
+        EnumSingleton.INSTANCE.disply();
+        EnumSingleton.INSTANCE.disply();
+        EnumSingleton.INSTANCE.disply();
+        
+        System.out.println();
     }
 
 }
